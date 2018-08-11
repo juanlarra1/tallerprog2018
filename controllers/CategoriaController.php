20aae29d4a2be95100c97767685be26dbe43dda4
CarmenBarreira <csilbarreira@gmail.com>
2018-08-11 12:35:04 -0300

    alta categoria

--- controllers/CategoriaController.php
+++ controllers/CategoriaController.php
@@ -1,16 +1,33 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+require_once('librerias/smarty/libs/Smarty.class.php');
+require_once('BaseController.php');
+require_once('models/CategoriaModel.php');
+require_once('librerias/seguridad.php');
 
-/**
- * Description of CategoriaController
- *
- * @author Larra
- */
-class CategoriaController extends BaseController{
-    //put your code here
+class CategoriaController extends BaseController {
+
+    function RegistroAction() {
+        $categoriaModel = new CategoriaModel();
+        if (!empty($_POST)) {
+            $nombre = xss_clean($_POST['txtNombreCategoria']);
+            $eliminado = xss_clean(isset($_POST['chkCategoriaEliminada']) ? 1 : 0);
+
+
+            $categoriaModel->nombre = $nombre;
+            $categoriaModel->eliminado = $eliminado;
+
+            if ($categoriaModel->validarCategoria()==null) {
+
+                $categoriaModel->crearCategoria();
+                header('Location: index.php?categoria/index');
+                exit;
+            }
+        }
+        $control = array("categoriaModel" => $categoriaModel);
+
+
+        $this->render("registroCategoria", $control);
+    }
+
 }
--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -46,14 +46,4 @@ class SiteController extends BaseController {
     }
 }
 
-/* $user = "usuario@prueba.com";
-  $password = "32250170a0dca92d53ec9624f336ca24";
-
-  $userModel = new UsuarioModel();
-
-  $login = $userModel->checkLogin($user, $password);
-  if ($login === false) {
-  $this->jsonEncode(["mensaje" => "Credenciales Incorrectas"]);
-  }
-
 
